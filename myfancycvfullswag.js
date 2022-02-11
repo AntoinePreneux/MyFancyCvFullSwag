705560442e9ab2505904fd2cfab887c1f1078a3e
AntoinePreneux <91544746+AntoinePreneux@users.noreply.github.com>
2022-02-11 16:36:23 +0100

    Add files via upload

---
+++ myfancycvfullswag.js
@@ -0,0 +1,8 @@
+//Affecte la nouvelle image lorsque la souris survole l'élément
+function passageDeLaSouris(element) {
+    element.setAttribute('src', '6BD07BA8-0345-41A2-8622-F159E741F88F_4_5005_c.jpeg');
+    }
+//Affecte l'image de départ lorsque la souris ne survole plus l'élément
+function departDeLaSouris(element) {
+    element.setAttribute('src', '9642d6fb-d7e4-4fec-b5c5-81b46c43f240.jpg');
+    }
