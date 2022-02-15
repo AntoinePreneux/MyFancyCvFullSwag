e67eb20a09185cfdb7fac3f34d5f6f683a5a44b6
Antoine Preneux <tonio@Air-de-Antoine.rezal-mdm.com>
2022-02-15 21:25:12 +0100

    cest zinzin

--- myfancycvfullswag.js
+++ myfancycvfullswag.js
@@ -7,22 +7,46 @@ function RetourneTete(element) {
     element.setAttribute('src', 'IMG_4413.jpeg');
     }
 
-WIDTH = 50
+WIDTH_eng = 10
+WIDTH_html = 10
 STEP = 100
 
-function grow_() {
-  	if (WIDTH <= 299) {
-      let growing = document.getElementById("growing")
+function grow_english() {
+  	if (WIDTH_eng <= 200) {
+      let growing = document.getElementById("english")
+    growing.style.width = `${WIDTH_eng}px`
+    WIDTH_eng += STEP
+    }
+    if (WIDTH_html <= 300) {
+        let growing = document.getElementById("html")
     growing.style.width = `${WIDTH}px`
     WIDTH += STEP
-    }
+  }
     
 }
 
-// this will be called on loading the <body> tag
-function grow_forever() {
-    // call it once right away
-    grow()
-    // get it called every 1s after that
-    setInterval(grow, 1000)
+function grow_CSSHTMLJS() {
+    if (WIDTH_html <= 300) {
+        let growing = document.getElementById("html")
+    growing.style.width = `${WIDTH}px`
+    WIDTH += STEP
+  }
+  
 }
+
+function grow_python() {
+    if (WIDTH <= 50) {
+        let growing = document.getElementById("python")
+    growing.style.width = `${WIDTH}px`
+    WIDTH += STEP
+  }
+  
+}
+
+// // this will be called on loading the <body> tag
+// function grow_forever() {
+//     // call it once right away
+//     grow_english()
+//     // get it called every 1s after that
+//     setInterval(grow_english, 1)
+// }
